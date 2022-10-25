98003ddb64ccfb91ac7cf24d07569a2e1538611d
PetrosguirguisM <82472385+PetrosguirguisM@users.noreply.github.com>
2022-10-25 20:35:24 +0200

    Add files via upload

---
+++ javascripts.js
@@ -0,0 +1,11 @@
+const side_bar = doctument.queryselector('.side_bar')
+
+
+
+function open_side (){
+    side_bar.style.visibilty = 'Visible';
+}
+
+function close_side (){
+    side_bar.style.visibilty = 'hidden';
+}
